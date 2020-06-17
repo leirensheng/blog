53c3f5a484574d6653ec5563321a6ca97239e824
leirensheng <leirensheng@163.com>
2020-06-17 09:06:37 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:leirensheng/blob.git master:gh-pages
+git push -f git@github.com:leirensheng/blog.git master:gh-pages
 
 cd -
